89b209edd176918dac6a2eb870172ca72cbae262
eannchen <conspivit@gmail.com>
2026-01-03 11:29:20 +0800

    leetsolv 1.2.0 (new formula)

---
+++ Formula/l/leetsolv.rb
@@ -0,0 +1,29 @@
+class Leetsolv < Formula
+  desc "CLI tool for DSA problem revision with spaced repetition"
+  homepage "https://github.com/eannchen/leetsolv"
+  url "https://github.com/eannchen/leetsolv/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "430303725aed7340f70d09e2461a82806e9f8ee195a98b8f0af6f0fc09236cd2"
+  license "MIT"
+  head "https://github.com/eannchen/leetsolv.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X main.Version=#{version}
+    ]
+    system "go", "build", *std_go_args(ldflags:)
+  end
+
+  test do
+    # Test status command shows initial state
+    output = shell_output("#{bin}/leetsolv status")
+    assert_match "Total Questions:", output
+
+    # Add a problem and verify it was added
+    system bin/"leetsolv", "add", "https://leetcode.com/problems/two-sum"
+    output = shell_output("#{bin}/leetsolv status")
+    assert_match "Total Questions: 1", output
+  end
+end
